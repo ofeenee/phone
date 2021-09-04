a28cff9e50d14740e480400347f29f6bf805af9b
ofeenee <ofeenee@gmail.com>
2021-09-04 18:31:36 +0300

    move to npm setup

---
+++ index.js
@@ -0,0 +1,57 @@
+import isMobilePhone from 'validator/lib/isMobilePhone.js';
+
+class Phone {
+  #phone;
+
+  constructor(phone) {
+    try {
+      if (isMobilePhone(phone, 'any', {strictMode: true})) {
+        this.#phone = phone;
+      }
+      else {
+        throw new Error('Phone value is invalid.');
+      }
+
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  static validate(phone) {
+    if (!isMobilePhone(phone, 'any', {strictMode: true})) {
+      throw new Error('Phone value is invalid.');
+    }
+    else {
+      return true;
+    }
+  }
+
+  update(phone) {
+    try {
+      if (isMobilePhone(phone, 'any', {strictMode: true}) && phone !== this.#phone) {
+        this.#phone = phone;
+      }
+      else if (this.#phone === phone) {
+        throw new Error('update is not necessary.');
+      }
+      else {
+        throw new Error('Phone value is invalid.');
+      }
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  get() {
+    try {
+      return this.#phone;
+    }
+    catch (error) {
+      console.log(error.message);
+    }
+  }
+}
+
+export default Phone;
