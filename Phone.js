f5da6e51f8b2b834b7074b46da0274a21afb6dd0
ofeenee <ofeenee@gmail.com>
2021-09-11 19:44:12 +0300

    rewrite Email.js from class to prototype

--- Phone.js
+++ Phone.js
@@ -1,5 +1,15 @@
 import validator from 'validator';
-const {isMobilePhone} = validator;
+const { isMobilePhone } = validator;
+
+function validatePhone(string) {
+  try {
+    if (typeof string !== 'string' || !string) throw new Error('value is invalid.');
+    return isMobilePhone(string, 'any', { strictMode: true });
+  }
+  catch (error) {
+    throw error;
+  }
+}
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -13,99 +23,125 @@ const {
 import twilio from 'twilio';
 const client = twilio(accountSid, authToken);
 
-class Phone {
-  #phone;
-  #verified = false;
-
-  constructor() {
-    try {
-      this.#phone = null;
-    }
-    catch (error) {
-      throw error;
-    }
-  }
-
-  static validate(phone) {
-    if (isMobilePhone(phone, 'any', {strictMode: true})) return true;
-    else return false;
-  }
+function Phone() {
+  try {
+    if (new.target === undefined) return new Phone();
+    let phone = null;
+    let verified = false;
 
 
-  set(phone) {
-    try {
-      if (!isMobilePhone(phone, 'any', { strictMode: true })) {
-        throw new Error('phone value is invalid.');
+    return Object.defineProperties(this, {
+      validate: {
+        value: validatePhone,
+        enumerable: true
+      },
+      phoneNumber: {
+        set: function setPhoneNumber(string) {
+          try {
+            if (!validatePhone(string)) {
+              throw new Error('phone value is invalid.');
+            }
+            else if (string === phone) {
+              return phone;
+            }
+            else {
+              phone = string;
+              return phone;
+            }
+
+          } catch (error) {
+            throw error;
+          }
+        },
+        get: function getPhoneNumber() {
+          if (phone) return phone;
+          else return null;
+        },
+        enumerable: true
+      },
+      verified: {
+        set: function (boolean) {
+          try {
+            if (typeof boolean !== 'string' || !boolean) throw new Error('value is invalid.');
+
+            if (boolean === verified) {
+              return verified;
+            }
+            else {
+              verified = boolean;
+              return verified;
+            }
+          }
+          catch (error) {
+            throw error;
+          }
+        },
+        get: function () {
+          if (typeof verified === 'boolean') return verified;
+          else return null;
+        },
+        enumerable: true
+      },
+      verifySMS: {
+        value: async function sendPhoneVerificationCodeBySMS() {
+          try {
+            if (!phone) throw new Error('phone is not yet set.');
+
+            const verification = await client.verify.services(serviceSid)
+              .verifications
+              .create({ to: phone, channel: 'sms' });
+
+            return verification;
+
+          }
+          catch (error) {
+            throw error;
+          }
+        },
+        enumerable: true
+      },
+      verifyCall: {
+        value: async function sendPhoneVerificationCodeByCall() {
+          try {
+            if (!phone) throw new Error('phone is not yet set.');
+
+            const verification = await client.verify.services(serviceSid)
+              .verifications
+              .create({ to: phone, channel: 'call' });
+
+            return verification;
+
+          }
+          catch (error) {
+            throw error;
+          }
+        },
+        enumerable: true
+      },
+      confirm: {
+        value: async function confirmPhoneVerificationCode(code) {
+          try {
+            if (!code) throw new Error('value is invalid.');
+
+            const verification = await client.verify.services(serviceSid)
+              .verificationChecks
+              .create({ to: phone, code });
+
+            const { valid } = verification;
+            if (valid) verified = true;
+            return verification;
+          }
+          catch (error) {
+            throw error;
+          }
+        },
+        enumerable: true
       }
-      else if (phone === this.#phone) {
-        return this.#phone;
-      }
-      else {
-        this.#phone = phone;
-        return this.#phone;
-      }
-    }
-    catch (error) {
-      throw error;
-    }
+    });
   }
-
-  get() {
-    try {
-      if (this.#phone) return this.#phone;
-      else return null;
-    }
-    catch (error) {
-      throw error;
-    }
-  }
-
-  async sendSMSPhoneVerification() {
-    try {
-      const verification = await client.verify.services(serviceSid)
-        .verifications
-        .create({ to: this.#phone, channel: 'sms' });
-
-      return verification;
-
-    }
-    catch (error) {
-      console.log(error.message);
-    }
-  }
-  async sendVoiceCallPhoneVerification() {
-    try {
-      const verification = await client.verify.services(serviceSid)
-        .verifications
-        .create({ to: this.#phone, channel: 'call' });
-
-      return verification;
-
-    }
-    catch (error) {
-      console.log(error.message);
-    }
-  }
-  async confirmPhoneVerification(code) {
-    try {
-      const verification = await client.verify.services(serviceSid)
-        .verificationChecks
-        .create({ to: this.#phone, code });
-
-      const { valid } = verification;
-      if (valid) this.#verified = true;
-      return verification;
-
-    }
-    catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  isVerified() {
-    return this.#verified;
+  catch (error) {
+    throw error;
   }
 }
-
 
 export default Phone;
